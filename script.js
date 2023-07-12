296429b63c87d45aa9e5cc937fa4e72628058d95
Mikhail <m79940601516@yandex.ru>
2023-07-12 22:38:04 +1000

    work on bugs

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-let numberOfFilms = prompt(`Сколько фильмов вы уже посмотрели?`);
-
-let lastFilm = prompt(`Какой один из последних просмотренных фильмов вы назовете?`)
-let reitFilm = prompt(`Какую оценку он заслужил?`)
+const numberOfFilms = prompt(`Сколько фильмов вы уже посмотрели?`);
 
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-        lastFilm: reitFilm
-    },
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+const a = prompt(`Какой один из последних просмотренных фильмов вы назовете?`),
+      b = prompt(`Какую оценку он заслужил?`),
+      c = prompt(`Какой один из последних просмотренных фильмов вы назовете?`),
+      d = prompt(`Какую оценку он заслужил?`);
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
