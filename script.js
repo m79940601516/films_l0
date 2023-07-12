0c605a3312c82306a50e2aae724dc4380f2a0fa5
Mikhail <m79940601516@yandex.ru>
2023-07-12 22:28:55 +1000

    add new questions

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let numberOfFilms = prompt(`Сколько фильмов вы уже посмотрели?`);
 
+let lastFilm = prompt(`Какой один из последних просмотренных фильмов вы назовете?`)
+let reitFilm = prompt(`Какую оценку он заслужил?`)
+
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {},
+    movies: {
+        lastFilm: reitFilm
+    },
     actors: {},
     genres: [],
     privat: false
