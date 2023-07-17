10cc291c72d4729fa3974b556749da8a7d0363de
Mikhail <m79940601516@yandex.ru>
2023-07-17 22:30:58 +1000

    add function code

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-const numberOfFilms = prompt(`Сколько фильмов вы уже посмотрели?`);
+let numberOfFilms
+
+function start(){
+    numberOfFilms = +prompt(`Сколько фильмов вы уже посмотрели?`);
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt(`Сколько фильмов вы уже посмотрели?`);
+    }
+}
+
+function rememberMyFilms(){
+    for(let i = 0; i < 2; i++){
+        const a = prompt(`Какой один из последних просмотренных фильмов вы назовете?`, '');
+        if(a == '' || a == null || a.length > 50){
+            i--
+        }else{
+            const b = prompt(`Какую оценку он заслужил?`);
+            personalMovieDB.movies[a] = b;
+        }
+    }   
+}
+function detectPersonalLevel(){
+    if(+personalMovieDB.count < 10){
+        alert('Просмотрено довольно мало фильмов')
+    }else if(+personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        alert('Вы классический зритель')
+    }else{
+        alert('Вы киноман')
+    }
+}
+function showMyDB(){
+    if(personalMovieDB.privat == false){
+        console.log(personalMovieDB)
+    }
+}
+function writeYourGenres(){
+    for(let i = 0; i < 3; i++){
+        personalMovieDB.genres[i] = +prompt("Ваш любимый жанр под номером ${номер по порядку}")
+    }
+}
+
+
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -8,15 +49,11 @@ let personalMovieDB = {
     privat: false
 };
 
-for(let i = 0; i < 2; i++){
 
-}
-const a = prompt(`Какой один из последних просмотренных фильмов вы назовете?`),
-      b = prompt(`Какую оценку он заслужил?`),
-      c = prompt(`Какой один из последних просмотренных фильмов вы назовете?`),
-      d = prompt(`Какую оценку он заслужил?`);
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+start();
+rememberMyFilms();
+detectPersonalLevel()
+writeYourGenres()
+showMyDB()
 
-console.log(personalMovieDB)
